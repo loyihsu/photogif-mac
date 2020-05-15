520a6d9505b12c0b6d6e82d4baa6a4de1cc6a0f6
Loyi Hsu <loyi.hsu@gmail.com>
2020-05-15 20:51:27 +0800

    Initial Commit

---
+++ 64-GIFs/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  64-GIFs
+//
+//  Created by Loyi Hsu on 2020/5/15.
+//  Copyright © 2020 Loyi Hsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
