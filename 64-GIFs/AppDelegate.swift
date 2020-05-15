453a7d2a5b2ce1382341848e24aa553464c6d922
Loyi Hsu <loyi.hsu@gmail.com>
2020-05-16 02:53:30 +0800

    Initial setup and record current bugs.

--- 64-GIFs/AppDelegate.swift
+++ 64-GIFs/AppDelegate.swift
@@ -14,15 +14,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        
+        //let contentView = ContentView()
 
-        // Create the window and set the content view. 
+        let contentView = ContentView.init(sources: [
+        "/Users/loyihsu/Downloads/aa/01.png",
+        "/Users/loyihsu/Downloads/aa/02.png",
+        "/Users/loyihsu/Downloads/aa/03.png"
+        ], length: [ "1", "1", "1" ])
+        
+        // Create the window and set the content view.
         window = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            styleMask: [.titled, .closable, .miniaturizable, .fullSizeContentView],
             backing: .buffered, defer: false)
         window.center()
         window.setFrameAutosaveName("Main Window")
--- 64-GIFs/ContentView.swift
+++ 64-GIFs/ContentView.swift
@@ -8,16 +8,99 @@
 
 import SwiftUI
 
+extension String {
+    func lastElement() -> String {
+        return self.components(separatedBy: "/").last ?? "Image"
+    }
+}
+
+let outputPath = "/Users/loyihsu/Downloads/aa/" // For testing
+
 struct ContentView: View {
+    @State var sources: [String] = []
+    @State var length: [String] = []
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            ScrollView(.vertical) {
+                VStack {
+                    
+                    // Current implementation cannot handle remove events
+                    // Suggestion is to change it to id-based ForEach
+                    // Or find a way to access it from within
+                    
+                    // Drag & Drop
+                    
+                    ForEach(0..<sources.count) { index in
+                        HStack {
+                            Image.init(nsImage: NSImage.init(contentsOfFile: self.sources[index])!).resizable()
+                                .frame(width: 32, height: 32)
+                            
+                            Text(self.sources[index].lastElement())
+                
+                            TextField("Seconds", text: self.$length[index])
+                            Text("Seconds")
+                            
+                            Button.init("Remove") {
+                                //self.sources.remove(at: index)
+                                //self.length.remove(at: index)
+                            }
+                        }
+                        .frame(width: 480, height: 50)
+                    }
+                }
+            }
+            .frame(width: 480, height: 360, alignment: .topLeading)
+            .padding()
+            
+            Button("Generate") {
+                if generateGIFs(from: self.sources.map { NSImage.init(contentsOfFile: $0)! }, delays: self.length.map { Double($0)! }, docDirPath: outputPath, filename: "/test.gif") {
+                    print("Success")
+                } else {
+                    print("Failed")
+                }
+            }
+            .padding()
+            
+            // Right: add button
+        }
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+
+        ContentView.init(sources: [
+            "/Users/loyihsu/Downloads/aa/01.png",
+            "/Users/loyihsu/Downloads/aa/02.png",
+            "/Users/loyihsu/Downloads/aa/03.png"
+        ], length: [ "1", "1", "1" ])
     }
 }
+
+//struct ContentView_Previews_Empty: PreviewProvider {
+//    static var previews: some View {
+//        ContentView.init()
+//    }
+//}
+
+//struct ContentView_Previews_Long: PreviewProvider {
+//    static var previews: some View {
+//
+//        ContentView.init(sources: [
+//            "/Users/loyihsu/Downloads/aa/01.png",
+//            "/Users/loyihsu/Downloads/aa/02.png",
+//            "/Users/loyihsu/Downloads/aa/03.png",
+//            "/Users/loyihsu/Downloads/aa/01.png",
+//            "/Users/loyihsu/Downloads/aa/02.png",
+//            "/Users/loyihsu/Downloads/aa/03.png",
+//            "/Users/loyihsu/Downloads/aa/01.png",
+//            "/Users/loyihsu/Downloads/aa/02.png",
+//            "/Users/loyihsu/Downloads/aa/03.png",
+//            "/Users/loyihsu/Downloads/aa/01.png",
+//            "/Users/loyihsu/Downloads/aa/02.png",
+//            "/Users/loyihsu/Downloads/aa/03.png"
+//        ], length: [ "1", "1", "1","1", "1", "1","1", "1", "1","1", "1", "1" ])
+//    }
+//}
