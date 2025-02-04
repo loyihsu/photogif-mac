45e9c12784c0e9f9ce2c1169e20d017b53868e2e
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2025-02-04 21:28:37 +0800

    tests: add test bundle

---
+++ PhotoGIFTests/PhotoGIFTests.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoGIFTests.swift
+//  PhotoGIFTests
+//
+//  Created by Yu-Sung Loyi Hsu on 2025/2/4.
+//  Copyright © 2025 Loyi Hsu. All rights reserved.
+//
+
+import Testing
+
+struct PhotoGIFTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
